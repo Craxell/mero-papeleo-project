c78e664b73e9482e20a42871b4471baaa29c0980
Craxell <andresaltamirasanz@gmail.com>
2024-09-25 10:31:21 -0500

    - Added new methods on mongo_repository.py

--- Backend/domain/repositories/mongo_repository.py
+++ Backend/domain/repositories/mongo_repository.py
@@ -1,4 +1,5 @@
 from infrastructure.mongo_client import MongoDBClient
+from typing import List, Dict
 
 class MongoRepository:
     def __init__(self):
@@ -8,8 +9,26 @@ class MongoRepository:
         collection = self.mongoClient[collection_name]
         return collection.find_one(query)
 
+    def find_many(self, collection_name: str, query: dict = {}) -> List[dict]:
+        collection = self.mongoClient[collection_name]
+        return list(collection.find(query))
+
+    def find_all(self, collection_name: str) -> List[dict]:
+        collection = self.mongoClient[collection_name]
+        return list(collection.find({}))
+
     def insert_one(self, collection_name: str, document: dict):
         collection = self.mongoClient[collection_name]
         return collection.insert_one(document)
 
-    # Agrega otros métodos según sea necesario
+    def insert_many(self, collection_name: str, documents: List[dict]):
+        collection = self.mongoClient[collection_name]
+        return collection.insert_many(documents)
+
+    def update_one(self, collection_name: str, query: dict, update: dict):
+        collection = self.mongoClient[collection_name]
+        return collection.update_one(query, {'$set': update})
+
+    def delete_one(self, collection_name: str, query: dict):
+        collection = self.mongoClient[collection_name]
+        return collection.delete_one(query)
