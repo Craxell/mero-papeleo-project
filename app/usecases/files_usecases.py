6a3d1b698f08530a40b9722fc6e6b6d50de1c966
Craxell <andresaltamirasanz@gmail.com>
2024-10-11 02:38:00 -0500

    - Fix content de archivos

--- app/usecases/files_usecases.py
+++ app/usecases/files_usecases.py
@@ -20,35 +20,31 @@ class RAGService:
         return self.openai_adapter.generate_text(prompt=query, retrieval_context=context)
 
     def save_document(self, file: UploadFile) -> dict:
-        """Guarda el archivo en el sistema de archivos y en la base de datos."""
         file_name = file.filename
-
         os.makedirs('userdata', exist_ok=True)
-
         file_path = os.path.join('userdata', file_name)
-        try:
-            with open(file_path, 'wb') as f:
-                f.write(file.file.read())
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error saving file: {str(e)}")
+        
+        # Guardar el archivo
+        with open(file_path, 'wb') as f:
+            f.write(file.file.read())
 
         document = Document(title=file_name, path=file_path)
 
-        # strategies
         try:
             content = FileReader(document.path).read_file()
             if isinstance(content, str) and (content.startswith("File not found") or content.startswith("An error occurred")):
-                return {"status": content}  # Devolver mensaje de error específico
-
-            self.mongo_repo.save_document(document)
-            self.document_repo.save_document(document, content, self.openai_adapter)
-
-            return {"status": "Document saved successfully"}
-
-        except ValueError as ve:
-            return {"status": str(ve)}
+                return {"status": content}
         except Exception as e:
             return {"status": str(e)}
+
+        document.content = content
+
+        # Guardar en la base de datos
+        self.mongo_repo.save_document(document)
+        self.document_repo.save_document(document, content, self.openai_adapter)
+
+        return {"status": "Document saved successfully"}
+
 
 
 
