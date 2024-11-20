28d8c502cdeca2ecfd0b075ef5a39820d2182ec8
Craxell <andresaltamirasanz@gmail.com>
2024-11-20 02:47:10 +0000

    - Update Locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,15 @@
 from locust import HttpUser, task, between
 import json
 
+
+import random
+import string 
+
+
 class ApiLoadTest(HttpUser):
     wait_time = between(1, 3)  # Shorter wait times for API testing
     token = None  # Store authentication token
-    
+
     def on_start(self):
         """Login and store authentication token"""
         login_payload = {
@@ -41,10 +46,14 @@ class ApiLoadTest(HttpUser):
     
     @task(2)
     def register_user(self):
+        username_lenght = random.randint(1,12)
+        username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=username_lenght))
+        email = f"{username}@locust.lc"
+        
         payload = {
-            "username": "testeoLOCUST3",
-            "password": "password123",
-            "email": "locust3@locust.lc"
+            "username": username,
+            "email": email,
+            "password": "password123"
         }
         with self.client.post(
             "/register",
@@ -97,18 +106,6 @@ class ApiLoadTest(HttpUser):
                 response.success()
             else:
                 response.failure(f"Get document failed: {response.status_code}")
-
-    @task(1)
-    def get_vectors(self):
-        with self.client.get(
-            "/get-vectors",
-            headers=self.get_auth_headers(),
-            catch_response=True
-        ) as response:
-            if response.status_code == 200:
-                response.success()
-            else:
-                response.failure(f"Get vectors failed: {response.status_code}")
 
     @task(1)
     def get_users(self):
